e2210a7d6f6c80151e6f86720900527db2d8260e
Lalith Amudala <lalithamudala7@gmail.com>
2026-02-27 16:23:07 -0600

    Updated workflow

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from dotenv import load_dotenv
+
 load_dotenv()
 
+from graph.graph import app
+
 if __name__ == "__main__":
-    print("Hello Advanced RAG!")
+    print("Hello Advanced RAG")
+    print("Hello Advanced RAG")
+    print(app.invoke(input={"question": "what is agent memory?"}))
