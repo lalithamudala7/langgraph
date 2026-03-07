23ef6f6c8dd8e3435e63d2f0bec8b040c6704983
Lalith Amudala <lalithamudala7@gmail.com>
2026-03-07 15:15:53 -0600

    Adaptive RAG

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ from graph.graph import app
 if __name__ == "__main__":
     print("Hello Advanced RAG")
     print("Hello Advanced RAG")
-    print(app.invoke(input={"question": "what is agent memory?"}))
+    print(app.invoke(input={"question": "agent memory?"}))
